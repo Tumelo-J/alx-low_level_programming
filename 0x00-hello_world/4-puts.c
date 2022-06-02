df97263b102fe5af6b172f3a0695ade1a086ed56
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-02 15:10:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(void)
-{	/*This prints a simple string to console*/
-	puts("Programming is like building a multilingual puzzle")
-	return (0)
+{
+	puts("Programming is like building a multilingual puzzle");/*Prints string to console*/
+	return (0);
 }
