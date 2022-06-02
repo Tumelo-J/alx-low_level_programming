ba1a74bfa8c8dff28b7ccc159a0177a72003ff83
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-02 16:11:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - Code entry code 
- * Return: 0
+ *main-Code entry code
+ *Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
