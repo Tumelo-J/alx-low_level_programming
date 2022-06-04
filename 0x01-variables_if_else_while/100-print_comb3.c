96ca9cfd652be2a9ade79aa4011b95db1f32a0d5
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-03 21:34:58 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int i;
 	for (i = 0; i < 100; i++)
-		putchar((i / 10) + '0')	
-		putchar((i % 10) + '0')	
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 	putchar('\n');
 	return (0);
 }
