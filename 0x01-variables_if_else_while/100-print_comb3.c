61355490c4ebb850efdfb89f58bb239a4aaa0d2f
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-03 21:21:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,38 +7,14 @@
  */
 int main(void)
 {
-	int i = 0;
-	int y;
-	int x;
-
-	while (i < 10)
-	{
-		x = 0;
-		while (x < 10)
-		{
-			y = 0;
-			while (y < 10)
-			{
-				if (y != x && x != i && i < x && x < y)
-				{
-					putchar('0' + i);
-					putchar('0' + x);
-					putchar('0' + y);
-
-					if (y + x + i != 9 + 8 + 7)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				y++;
-			}
-			x++;
-		}
-		i++;
-	}
-
+	int i;
+	int j;
+	char ic = itoa(i);
+	char jc = itoa(j);
+	for (i ='0';i <= '9'; i++;
+			for (j = '0'; j<='9'; j++)
+				if (i != j && "icjc" != "jcic")
+					putchar(ij)
 	putchar('\n');
-
 	return (0);
 }
