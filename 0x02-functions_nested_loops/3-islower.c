20adf2bc38698a08a3660419f578410e4b136694
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-07 15:45:01 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _islower() -User Function
+  * Description: Determines if input is lowercase or uppercase.
+  * Return: 1 if lowercase and 0 if other
+ */
+int _islower(int c)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (c == ch)
+			return (1);
+		else
+			return (0);
+}
