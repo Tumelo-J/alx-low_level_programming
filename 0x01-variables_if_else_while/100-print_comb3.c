666f6547c4e279fdfd8e737c599ab0004cc20a4b
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-06 20:23:13 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,15 @@
 int main(void)
 {
 	int i;
-	
+	char str[3];
+
 	for (i = 0; i < 100; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
+		if (sprintf(str,"%d",i)[0] != sprintf(str,"%d",i)[1])
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+		}
 	}
 	putchar('\n');
 	return (0);
