9b89872571e4e847d9b3695c1a6e88497247966e
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-07 14:35:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,12 @@
   * main -Entry point
   * return: 0
  */
+char str[9] = "_putchar";
+
 int main(void)
 {
-	_putchar(_putchar);
+	for (i = 0; i < 9 , i++)
+		_putchar(str[i]);
+	_putchar('\n');
 	return (0);
 }
