a370267ec5cc09ee12e692e45143d53f5077c827
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-07 17:43:02 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * int_abs-User Function
+  * @n: num to be made absolute
+  * Return: absolute n 
+ */
+int int_abs(int n)
+{
+	int num = (n < 0) ? -n : n;
+
+	return (num)
+}
