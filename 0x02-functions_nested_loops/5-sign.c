63aca7a5d528b7b714cd2bcf40a35776f74e75ce
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-07 17:31:57 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * print_sign -User Function
+  * @n: Character to be checked if positive, zero or negative
+  * Return: 1 if +, 0 if 0 and -1 if -
+ */
+int print_sign(int n)
+{
+	int num;
+
+	if (n < 0)
+	{
+		num = -1;
+		_putchar('-');
+	}
+	else if (n == 0)
+	{
+		num = 0;
+		_putchar('0');
+	else if (n > 0)
+	{
+		num = 1;
+		_putchar('+');
+
+	return (num);
+}
