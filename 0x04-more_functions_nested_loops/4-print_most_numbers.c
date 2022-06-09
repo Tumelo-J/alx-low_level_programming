d99004209453c1a083e63918d0fe20b2b93ad7cc
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-09 16:41:10 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = '0'; (i <= '9') && (i != '2') && (i != '4'); i++)
-		_putchar(i);
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
 	_putchar('\n');
 }
