46270eb5d5ca93ced679c722b02d62b702ecc382
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-09 17:02:01 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,11 @@
 */
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = '0'; i <= '9'; i++)
 				_putchar(i);
 	}
 	_putchar('\n');
