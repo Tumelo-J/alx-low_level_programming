2f2c3f3288f6d2e90b7d0e7f6599b57bd51a3a9f
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-13 19:34:59 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; (str[i] != '\0') && (i%2 == 0); i++)
 	{
 		_putchar(str[i]);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
