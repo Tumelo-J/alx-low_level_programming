ef42ddb63b1d0568ddf4848f3f361a62eeec1ad5
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-13 18:43:49 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/3-put.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+  * _puts -Prints a string to stdout
+  * @str: Input string
+  * Return: void
+  */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
