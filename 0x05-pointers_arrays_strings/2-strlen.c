1d6c9a6f5ea521c6e3749ec498ffff23c9c55f6d
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-13 18:37:43 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,8 +7,9 @@
   */
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 	/* s is the location of the input string in memory => s =&string[0] */
-	for (length = 0; s[length] != '\0';length++)
+	while (s[length] != '\0')
+		length++;
 	return (length);
 }
