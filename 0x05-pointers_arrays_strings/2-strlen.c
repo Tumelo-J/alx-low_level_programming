19ace816f43415121518123f8f0ae0b15e19bdf5
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-13 17:51:06 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _strlen -Finds length of input string, similar to strlen
+  * @s: Input string
+  * Return: Length of string, int
+  */
+int _strlen(char *s)
+{
+	int n = sizeof(*s);
+	return (n);
+}
