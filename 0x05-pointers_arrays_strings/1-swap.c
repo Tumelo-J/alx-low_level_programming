4513ab251e063326b6995c915ccdfe119dd7e295
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-13 17:35:32 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * swap_int- Swaps values of inputs
+  * @a: Input integer
+  * @b: Input integer
+  * Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	a = &b;
+	b = &a
+}
