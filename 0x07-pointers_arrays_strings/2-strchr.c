f802d03f4b7c2e0186ef1cb769de6ea7b141f083
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-20 18:39:39 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strchr - Function checks the occurance of a character in a string
+  * @s: String from which occurance of c is checked
+  * @c: The character whose occurance in s is checked
+  *
+  * Return: Pointer to the first occurance of character c in s or NULL if not found
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return(s + i);
+	}
+	return (0);
+}
