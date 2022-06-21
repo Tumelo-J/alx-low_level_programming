ff5a99e30b34cacd4a27bb040a8507424fd5c64e
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-21 06:30:59 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strspn - Function finds segment memory starting at memory address 'accept'
+ * @s: Memory segment
+ * @accept: Starting memory address 
+ *
+ * Return: Number of bytes
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int n = 0;
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] = accept)
+			n++;
+	}
+	return (n)
+}
