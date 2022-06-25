0645d89b6ee6191ea44a922f38496b9b4b108bb2
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-24 19:41:34 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,9 @@
   * Return: 0
   */
 
-int main(int argc, char **argv)
-{	argv[argc] = 'Null';
-	argc--;
-	printf("%d\n", argc);
+int main(int argc, char const **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
 	return (0);
 }
 
