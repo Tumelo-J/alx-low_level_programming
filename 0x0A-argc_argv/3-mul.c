94ab91b0e9fd588eec79ef29277d8121a065ec67
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-24 19:54:13 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Main function
+  * @argc: Number of arguments passed in the command line
+  * @argv: Array containing arguments passed.
+  * Return: 0
+  */
+
+int main(int argc, char **argv)/* **argv is the same as *argv[]*/
+{
+	if (argc ==  3)
+	{
+		printf("%d\n",(*argv + 1)*(argv +2));
+		return (0)
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
+
