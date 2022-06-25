705b2fd40500e1c47f77a94329bd2efb0b65c0b3
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-24 20:12:37 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)/* **argv is the same as *argv[]*/
 {
 	if (argc ==  3)
 	{
-		printf("%d\n", atoi(*argv + 1) * atoi(*argv + 2));
+		printf("%d\n", atoi((*argv + 1)) * atoi((*argv + 2)));
 		return (0);
 	}
 	else
