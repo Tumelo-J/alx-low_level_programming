fb73a310bbee046e1fd065855fbf58fd7a15c3bb
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-24 20:25:34 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,10 @@ int main(int argc, char *argv[])
 {
 	if (argc ==  3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(arg[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+	printf("Error\n");
+	return (1);
 }
 
