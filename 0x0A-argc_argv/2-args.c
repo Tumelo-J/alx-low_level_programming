10904aca2941b215e5b661d7a674a6ad80932d19
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-24 19:07:01 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Main function
+  * @argc: Number of arguments passed in the command line
+  * @argv: Array containing arguments passed.
+  * Return: 0
+  */
+
+int main(int argc, char **argv)/* **argv is the same as *argv[]*/
+{
+	while(--argc)
+	{
+		printf("%s\n", argv[argc]);
+	}
+	return (0);
+}
+
