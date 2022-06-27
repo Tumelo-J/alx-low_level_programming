2912199a1343092a3826ff9c133a3dbd29e744c6
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-26 18:50:57 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strcat - Concatinates two strings
+  * @dest: Original string
+  * @src: String to be appended to dest
+  *
+  * Return: New dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j =0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	return (dest)
+}
