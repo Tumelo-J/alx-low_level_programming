788899ccdbd98d327aaf2dcb553ce25425b7d3c5
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-26 19:33:35 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * _strncpy - Copies src to dest
+  * @dest: New string
+  * @src: String to be copied
+  * @n: Number of bytes of src to be concatinated with dest
+  *
+  * Return: New dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (j < n)
+	{
+		if (src[j] != '\0')
+		{
+			dest[j] = src[j];
+			j++;
+		}
+		else
+			return (dest);
+	}
+	return (dest);
+}
