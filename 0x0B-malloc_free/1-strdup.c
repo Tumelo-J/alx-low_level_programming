255ac668488cff5b795985bb95eda04a3541143a
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-27 21:39:06 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 	char dupstr;
 	char *p;
 
-	dupstr[(char *) malloc(sizeof(str))] = *str;
+	dupstr[(char *) malloc(sizeof(str))] = **str;
 	if (dupstr == NULL || str == NULL)
 		return (NULL);
 	p = &dupstr;
