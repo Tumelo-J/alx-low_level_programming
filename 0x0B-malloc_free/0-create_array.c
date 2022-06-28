3b33f5e24d38b6985a3b9ec8673753c14c791279
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-27 20:56:29 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,10 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return (NULL);
-	char *str;
+	int *str;
 	unsigned int i = 0;
 
-	str = (char *) malloc(sizeof(char) * size);
+	str = malloc(sizeof(char) * size);
 	if (str != NULL)
 	{
 		for (i = 0; i < size; i++)
