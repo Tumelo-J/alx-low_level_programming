e206fc8757e8d7b06ea558076894fcd2954f1a45
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-27 21:26:11 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup  - Function duplicates a string
+  * @str: String to be duplicated
+  *
+  * Return: Pointer to array | NULL if size = 0 or program fails
+  */
+
+char *_strdup(char *str)
+{
+	char dupstr;
+	char *p;
+
+	dupstr[malloc(sizeof(str)] = str;
+	if (dupstr == NULL || str == NULL)
+		return (NULL);
+	p = &dupstr;
+	return (p);
+}
