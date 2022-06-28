6e5de69bfb3425f24f4713505a931a7c80cd6441
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-06-27 19:50:08 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	int i = 0;
 
 	if (size == 0)
 		return (NULL);
 	str = malloc(size);
-	str[] = c;
 
+	while (i < size)
+		str[i] = c;
 	return (str);
 }
