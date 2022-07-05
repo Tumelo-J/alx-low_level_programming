695a3759f12cf830c7bc20be2baa7105e237a300
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-04 18:21:43 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
-#define DOG_H 
-/** 
- * struct dog - Struct describes parameters of a dog 
- * @name: Name of the dog 
- * @age: Age of dog 
- * @owner: Owner of dog 
- */ 
+#define DOG_H
+/**
+ * struct dog - Struct describes parameters of a dog
+ * @name: Name of the dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
 struct dog
 {
 	char *name;
