5bdb30cff60f619fb8242e176c12a6cbc9b2bb6e
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-04 19:34:58 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+  * int_dog - Finction intializes a struct dog
+  * @d: Pointer to struct
+  * @name: Pointer to dog name
+  * @age: Dog age
+  * @owner: Dog owner
+  *
+  * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	typedef struct dog *d;
+}
