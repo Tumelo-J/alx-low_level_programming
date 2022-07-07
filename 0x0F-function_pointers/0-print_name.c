50a6ecd4441864d53ee4739ffa131d11c2463573
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-06 21:13:25 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
   * print_name - Function prints a name
   * @name: Poiter to name array
@@ -10,5 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(*name);
+	(*f)(name);
 }
