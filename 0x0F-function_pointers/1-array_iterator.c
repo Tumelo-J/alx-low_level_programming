c7485410527a8d4fe39b68c776b5386cb0c4dbd5
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-07 11:33:58 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,16 @@
 #include "function_pointers.h"
 /**
-  * array_iterator - Function runs function on array elements
-  * @action: Function Pointer
-  * @size: Array size
-  * @array: Array containing integers
-  * Return: Void
-  */
+* array_iterator - Function runs function on array elements
+* @action: Function Pointer
+* @size: Array size
+* @array: Array containing integers
+* Return: Void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+        while (size--)
+        {
+                action(*array);
+                array++;
+        }
 }
