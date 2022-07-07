f89c101d8bec77783b6929963e17057b92121eba
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-07 11:42:24 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
 * @array: Array containing integers
 * Return: Void
 */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, unsigned int size, void (*action)(int))
 {
 	while (size--)
 	{
