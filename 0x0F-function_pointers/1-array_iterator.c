d975507299de38dca88063c5d0e3dadae339a071
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-07 10:17:03 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
   * array_iterator - Function runs function on array elements
   * @action: Function Pointer
   * @size: Array size
-  * 
+  * @array: Array containing integers
   * Return: Void
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
