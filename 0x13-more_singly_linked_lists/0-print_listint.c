30b9342549158e25b20bb597b9a930e3a86a9976
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-20 20:32:50 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print_listint - Function prints all elements of listint
+  * @h: Pointer pinting to the next node
+  *
+  * Returnn: Number of nodes
+  */
+
+
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t.n = 0;
+
+	while (*h != NULL)
+	{
+		printf(*h);
+		(listint_t.n)++;
+	}
+	return (listint_t.n)
+}
