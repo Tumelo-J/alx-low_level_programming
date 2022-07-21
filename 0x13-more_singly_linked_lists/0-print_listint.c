21df9f9c779e11a96e7447164e989faafc99f809
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-20 21:19:31 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,10 +13,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while(h)
+	while (h)
 	{
-		printf("%d\n", h -> n);
-		h = h -> next;
+		printf("%d\n", h->n);
+		h = h->next;
 		i++;
 	}
 	return (i);
