5ca2a901465b354764c5a12a961141f73ebfce69
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-07-31 08:52:43 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t lread, fopened, fread, fwrite;
+	char *fsize;
+
+	/* Alloxating memery to enable reading the contents of the file into a character array */
+	fsize = malloc((letters + 1) * sizeof(char));
+	if (!filename || !fsize)
+		return (0);
+	fopened = open(filename, O_RONLY);
+	if (!fopened)
+		return (0);
+	fread = read(fopened, fsize, letters);
+	if (!fread)
+		return (0)
+	fwrite = write(STDOUT_FILENO, fsize, fread);
+	if (!fwrite)
+		return (0);
+	close(fopened);
+	free(fsize);
+	return (fwrite)
+}
