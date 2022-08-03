57e7be84361c1f3318a1206bcfd37754f34b3cbd
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-03 03:00:30 -0700

    justtesting

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, unsigned int size, void (*action)(int));
---
+++ Projects/Printf/README.md
@@ -0,0 +1,2 @@
+// This project is aimed at creating a replica of the built-in C function 'printf'
+
