b490e2e2254cab8924bd0106ebfafb4fd4c93d89
Tumelo J Mtsweni <TumeloJ.Mtsweni@gmail.com>
2022-08-15 17:16:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,28 +6,62 @@
  * @letters: number of letters
  * Return: the actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t lread, fopened, fread, fwrite;
-	char *fsize;
+    ssize_t file, fread, fwrite;
+    char *totalSize;
 
-	fsize = malloc((letters) * sizeof(char));
-	if (!filename || !fsize)
-	{
-		free(fsize);
-		return (0);
-	}
-	fopened = open(filename, O_RDONLY);
-	if (!fopened)
-		return (0);
-	fread = read(fopened, fsize, letters);
-	if (fread == -1)
-		return (0);
-	fwrite = write(STDOUT_FILENO, fsize, fread);
-	if (fwrite == -1)
-		return (0);
-	close(fopened);
-	free(fsize);
-	return (fwrite);
+    totalSize = malloc(sizeof(char) * letters);
+    if (totalSize == NULL)
+        return (0);
+    if (filename == NULL)
+        return (0);
+
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+        return (0);
+    fread = read(file, totalSize, letters);
+    if (fread == -1)
+        return (0);
+    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    if (fwrite == -1)
+        return (0);
+    close(file);
+    free(totalSize);
+    return (fwrite);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    ssize_t file, fread, fwrite;
+    char *totalSize;
+
+    totalSize = malloc(sizeof(char) * letters);
+    if (totalSize == NULL)
+        return (0);
+    if (filename == NULL)
+        return (0);
+
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+        return (0);
+    fread = read(file, totalSize, letters);
+    if (fread == -1)
+        return (0);
+    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    if (fwrite == -1)
+        return (0);
+    close(file);
+    free(totalSize);
+    return (fwrite);
 }
