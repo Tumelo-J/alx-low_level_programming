dc9eaf8146fce40beb313b40e3de2e5e379d72b4
Tumelo-Joel <TumeloJ.Mtsweni.com>
2022-09-24 06:13:17 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,34 +1,26 @@
 #include "hash_tables.h"
 
-#define T hash_table_t /* Hashtable */
-#define N hash_node_t  /* Node */
+#define T hash_table_t
+#define N hash_node_t
 
-/**
- * hash_table_create - Creates a hashtable
- * @size: size of the array
- * Return: A pointer to the hashtable or NULL if something went wrong
- */
-hash_table_t *hash_table_create(unsigned long int size)
+T *hash_table_create(unsigned long int size)
 {
-	T *table;
+    /* Memory allocation */
+    T *table = malloc(sizeof(T));
 
-	/* Memory allocation */
-	table = malloc(sizeof(T));
+    /* Return NULL if memory was not allocated */
+    if (table == NULL)
+        return (NULL);
+    
+    /* Initialize the hashtable */
+    table->size = size;
+    table->array = calloc(size, sizeof(N));
 
-	/* Return NULL if memory allocation was not done */
-	if (table == NULL)
-		return (NULL);
-
-	/* Value assignment */
-	table->size = size;
-	table->array = calloc(size, sizeof(N *));
-
-	/* Free memory if the array is NULL */
-	if (table->array == NULL)
-	{
-		free(table);
-		return (NULL);
-	}
-	return (table);
+    /* Free memory if array or size are NULL) */
+    if (table->array == NULL || table->size == 0)
+    {
+        free(table);
+        return (NULL);
+    }
+return (table);
 }
-
