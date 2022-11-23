f0c9cc278b0ffdae9c25d4de3568a2956dd6473b
Tumelo-Joel <TumeloJ.Mtsweni.com>
2022-11-23 02:52:44 -0800

    .

---
+++ hash_table.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+#define T hash_table_t /* Hashtable */
+#define N hash_node_t  /* Node */
+
+/**
+ * hash_table_create - Creates a hashtable
+ * @size: size of the array
+ * Return: A pointer to the hashtable or NULL if something went wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	T *table;
+
+	/* Memory allocation */
+	table = malloc(sizeof(T));
+
+	/* Return NULL if memory allocation was not done */
+	if (!table)
+		return (NULL);
+
+	/* Value assignment */
+	table->size = size;
+	table->array = calloc(size, sizeof(N *));
+
+	/* Free memory if the array is NULL */
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
+}
+
